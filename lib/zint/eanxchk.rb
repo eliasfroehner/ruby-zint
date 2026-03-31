6b53a26c33c070c977ac64ec2dcee84d3b6ee518
Lars Kanis <lars.kanis@sincnovation.com>
2026-03-31 09:11:06 +0200

    Print a deprecation warning for deprecated classes

--- lib/zint/eanxchk.rb
+++ lib/zint/eanxchk.rb
@@ -1,6 +1,13 @@
 module Zint
   # EAN + Check Digit
+  #
+  # Deprecated class -> use Ean8 or Ean13 instead.
   class EanxChk < Barcode
+    called = caller.find do |fn|
+      !fn.include?(RbConfig::TOPDIR) && !fn.start_with?(File.expand_path("..", __dir__))
+    end
+    warn("Zint::EanxChk is deprecated. Please use Ean8 or Ean13 instead! Called from #{called}", category: :deprecated)
+
     def initialize(value: nil, input_file: nil, **kwargs)
       super(value: value, input_file: input_file, symbology: Zint::BARCODE_EANX_CHK, **kwargs)
     end
--- spec/zint/eanx_spec.rb
+++ spec/zint/eanx_spec.rb
@@ -1,8 +1,18 @@
 module Zint
-  RSpec.describe Eanx do
+  RSpec.describe :Eanx do
+    if RUBY_VERSION >= "2.7."
+      before :all do
+        prev_deprecated = Warning[:deprecated]
+        Warning[:deprecated] = true
+        expect { Eanx }.to output(%r{deprecated.*spec/zint/eanx_spec}).to_stderr
+      ensure
+        Warning[:deprecated] = prev_deprecated
+      end
+    end
+
     describe "export" do
       it "export code" do
-        eanx_code = described_class.new(value: "012345678912")
+        eanx_code = Zint::Eanx.new(value: "012345678912")
         svg_file = eanx_code.to_memory_file(extension: ".svg")
 
         expect_svg_file(svg_file, "spec/fixtures/ean13.svg")
